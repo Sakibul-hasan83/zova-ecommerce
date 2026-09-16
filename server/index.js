419811f27d34ba4a05eb0cefcd339d712a05c0df
Sakibul Hasan <sakibulhasan@example.com>
2026-09-16 12:36:40 +0600

    Fix production CORS

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 8000;
 // Middleware
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: "https://zova-ecommerce.vercel.app",
     credentials: true,
   })
 );
